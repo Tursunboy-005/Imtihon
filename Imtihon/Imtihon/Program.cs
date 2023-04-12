304db53ed0018347bae940820c602a2828630d3c
Tursunboy <tursunboyrahimjonov1@gmail.com>
2023-04-12 11:54:49 +0500

    ToUpper string

--- Imtihon/Imtihon/Program.cs
+++ Imtihon/Imtihon/Program.cs
@@ -10,6 +10,15 @@ namespace Imtihon
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Matn kiriting ");
+
+            string str = Console.ReadLine();
+            str = str.Substring(0, str.Length - 2)  + str.Substring(in, 1).ToUpper();
+            Console.WriteLine(str);
+            string[] strings = str.Split(' ');
+
+            Console.ReadLine();
+
         }
     }
 }
